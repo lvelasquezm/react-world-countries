136eec9ec6e664419256eb9c9a033f500d554943
Lawrence Velasquez <law.vzmk@gmail.com>
2019-08-20 14:46:37 -0600

    Enhance folder structure

---
+++ src/containers/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>Hello World!</h1>
+    </header>
+  </div>
+);
+
+export default App;
